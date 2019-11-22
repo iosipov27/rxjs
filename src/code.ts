d6bdde7cb886d45aed1cfaee45bf5feda5a46c78
ilia <iosipov83@gmail.com>
2019-11-22 23:19:20 +0200

    Basic observer

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,30 @@
-import * as RX from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
-console.log(RX);
+
+const observable = Observable.create((observer: any) => {
+    observer.next('Hello Man');
+    observer.next('How are you?');
+    setInterval(() => {
+        observer.next('Not bad');
+    }, 1000)
+});
+
+
+function addItem(val: any) {
+    let node = document.createElement('li');
+    let text = document.createTextNode(val);
+    node.appendChild(text);
+    document.getElementById('output').appendChild(node);
+}
+
+
+const observer = observable.subscribe(
+    (item: any) => { addItem(item) },
+    (error: any) => { addItem(error) },
+    () => { addItem('Complete') }
+);
+
+setTimeout(() => {
+    observer.complete();
+    observer.unsubscribe();
+}, 5000)
